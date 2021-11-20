bd7dd9b298fa1b6d2643db74ec3bc3804e2be907
pamodasilva99 <96543@fhss.sjp.ac.lk>
2021-11-20 15:08:09 +0530

    Create Page01 UI

---
+++ app/screens/page01.js
@@ -0,0 +1,57 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+export default class login extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+      return (
+        <View>
+            <View style={styles.view1}>
+                <Image source={require('../assets/Image/pic8.png')} />
+            </View>
+            <Text style={styles.text1}> Are you ? </Text>
+            <View style={styles.view2}>
+                <TouchableOpacity style={styles.button} onPress={this.onPressDonor}>
+                    <Text style={styles.text2}> Donor </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={this.onPressFinder}>
+                    <Text style={styles.text2}> Blood Finder </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+    view1:{
+      alignItems: 'center',
+    },
+    text1: {
+        fontSize: 35,
+        marginTop: 50,
+        textAlign: 'center',
+        color: '#000',
+    },
+    view2: {
+        marginTop: 40,
+    },
+    button: {
+        borderWidth: 3,
+        borderRadius:30,
+        margin: 20,
+        width: 320,
+        backgroundColor: '#F67676',
+        marginLeft: 45,
+      },
+      text2: {
+        fontSize: 25,
+        textAlign: 'center',
+        margin: 7,
+        color: '#000',
+      },
+  });
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './app/screens/login';
+import App from './app/screens/page01';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
