e83a161f8233b9dd63344f8b559c2f614da7af7a
pamodasilva99 <96543@fhss.sjp.ac.lk>
2021-11-23 00:03:02 +0530

    Create Donors search UI

--- app/screens/DonorFinder.js
+++ app/screens/DonorFinder.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {TextInput, View, StyleSheet, Image, TouchableOpacity, Text, ScrollView} from 'react-native';
 
 export default class DonorFinder extends Component {
   constructor() {
@@ -9,11 +9,22 @@ export default class DonorFinder extends Component {
 
   render(){
     return (
-      <View>
+      <ScrollView>
         <View style={styles.view1}>
-          <Text style={styles.text1}> Donor Finder </Text>
+          <Image source={require('../assets/Image/pic5.png')} />
         </View>
-      </View>
+        <View style={styles.view2}>
+            <TextInput style={styles.input}
+            placeholder="Blood Type"
+            onChangeText = {this.handleBloodType}/>
+            <TextInput style={styles.input}
+            placeholder="City"
+            onChangeText = {this.handleCity}/>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text1}> Ok </Text>
+        </TouchableOpacity>
+      </ScrollView>
       );
   }
 }
@@ -22,10 +33,30 @@ const styles = StyleSheet.create({
   view1:{
     alignItems: 'center',
   },
-  text1: {
-    fontSize: 35,
-    marginTop: 50,
+  view2: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 2,
+    borderRadius:30,
+    margin:10,
+    width: 320,
     textAlign: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius:30,
+    margin:10,
+    width: 320,
+    backgroundColor: '#F67676',
+    marginLeft: 45,
+  },
+  text1: {
+    fontSize: 22,
+    textAlign: 'center',
+    margin: 5,
     color: '#000',
   },
 });
