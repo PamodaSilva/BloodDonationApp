cb7607719af7a3cec8225ff8341d8ae6c4cb3377
pamodasilva99 <96543@fhss.sjp.ac.lk>
2021-11-23 00:09:43 +0530

    Rename Donor Search Page

--- app/navigation/TabNavigation.js
+++ app/navigation/TabNavigation.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import DonorFinder from '../screens/DonorFinder';
+import DonorSearch from '../screens/DonorSearch';
 import Amount from '../screens/Amount';
 import About from '../screens/About';
 import profile from '../screens/profile';
@@ -12,7 +12,7 @@ class App extends Component {
     render() {
         return (
             <Tab.Navigator>
-                <Tab.Screen name="DonorFinder" component={DonorFinder}/>
+                <Tab.Screen name="DonorSearch" component={DonorSearch}/>
                 <Tab.Screen name="Amount" component={Amount}/>
                 <Tab.Screen name="About" component={About}/>
                 <Tab.Screen name="profile" component={profile}/>
