2ffb7c2d3467c391ae6fc1307a5c40e2461d0665
pamodasilva99 <96543@fhss.sjp.ac.lk>
2021-11-23 12:51:13 +0530

    Create blood donor  Amount UI page

--- app/screens/Amount.js
+++ app/screens/Amount.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 export default class Amount extends Component {
   constructor() {
@@ -11,7 +11,33 @@ export default class Amount extends Component {
     return (
       <View>
         <View style={styles.view1}>
-          <Text style={styles.text1}> Amount </Text>
+          <Image source={require('../assets/Image/pic6.png')}/>
+        </View>
+        <View style={styles.view2}>
+          <View>
+            <Text style={styles.text2}> A+ </Text>
+            <Text style={styles.text2}> B+ </Text>
+            <Text style={styles.text2}> AB+ </Text>
+            <Text style={styles.text2}> O+ </Text>
+          </View>
+          <View style={styles.view4}>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+          </View>
+          <View style={styles.view3}>
+            <Text style={styles.text2}> A- </Text>
+            <Text style={styles.text2}> B- </Text>
+            <Text style={styles.text2}> AB- </Text>
+            <Text style={styles.text2}> O- </Text>
+          </View>
+          <View style={styles.view4}>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+            <Text style={styles.number}> 00 </Text>
+          </View>
         </View>
       </View>
       );
@@ -22,10 +48,28 @@ const styles = StyleSheet.create({
   view1:{
     alignItems: 'center',
   },
-  text1: {
-    fontSize: 35,
-    marginTop: 50,
-    textAlign: 'center',
+  view2: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 45,
+  },
+  view4: {
+    marginLeft: -15,
+  },
+  view3: {
+    marginLeft: 20,
+  },
+  text2: {
+    fontSize: 30,
+    margin: 9,
+    marginBottom: 40,
     color: '#000',
+  },
+  number: {
+    fontSize: 30,
+    margin: 9,
+    marginBottom: 40,
   },
 });
