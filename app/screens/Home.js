f208b970cae48b2797b624f4b49d5fbc7fa34b5c
pamodasilva99 <96543@fhss.sjp.ac.lk>
2021-12-10 15:39:46 +0530

    Edit Home Page

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Image, Button} from 'react-native';
+import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 export default class Home extends Component {
   constructor() {
@@ -19,13 +19,13 @@ export default class Home extends Component {
           <Text style={styles.text2}> Save Life </Text>
         </View>
         <View style={styles.view3}>
-          <Image source={require('../assets/Image/logo1.png')} />
+          <Image source={require('../assets/Image/logo1.jpg')} style={styles.image}/>
         </View>
-        <View style={styles.button}>
-          <Button title="Login"
-            onPress={this.onPressLogin}
-            color="#F67676"/>
-        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress ={this.onPressLogin}>
+            <Text style={styles.text3}> LogIn </Text>
+        </TouchableOpacity>
         <View style={styles.view4}>
           <Image source={require('../assets/Image/pic1.png')} />
         </View>
@@ -37,6 +37,7 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   fullView: {
     alignItems: 'center',
+    backgroundColor: '#ffff',
   },
   view2: {
     marginTop: 70,
@@ -44,18 +45,34 @@ const styles = StyleSheet.create({
   text1: {
     fontSize: 25,
     textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#000',
   },
   text2: {
     fontSize: 20,
     textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#000',
   },
   view3: {
-    marginTop:85,
-    marginBottom: 80,
+    marginTop:50,
+    marginBottom: 55,
+  },
+  image: {
+    width: 120,
+    height: 150,
   },
   button: {
-      width: 90,
-      marginBottom: 10,
+    borderWidth: 2,
+    margin:10,
+    width: 120,
+    backgroundColor: '#FFF',
+  },
+  text3: {
+    fontSize: 23,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#000',
   },
   view4: {
     alignItems: 'center',
